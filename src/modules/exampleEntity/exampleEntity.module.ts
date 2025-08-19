2fea7e4263b08ec326fbf1d53a55f5000524ceae
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2025-08-19 16:21:34 -0300

    refactor(comments): remove unnecessary comments
    Co-authored-by: Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>

--- src/modules/exampleEntity/exampleEntity.module.ts
+++ src/modules/exampleEntity/exampleEntity.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-//import { IStudentRepository } from "./domain/student-repository.interface";
 import { EXAMPLE_ENTITY_REPOSITORY_TOKEN } from "./domain/exampleEntity-repository.interface";
 import { ExampleEntityontroller } from "./presentation/exampleEntity.controller";
 import { PrismaExampleEntityRepository } from "./infrastructure/exampleEntity.repository";
