e46a0ae1f33dba21298752de9fe47810e33f129b
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-08-30 16:16:17 -0300

    chore(auth): add FirebaseModule to AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExampleEntityModule } from './modules/exampleEntity/exampleEntity.module';
+import { FirebaseModule } from './modules/firebase/firebase.module';
 
 @Module({
   imports: [
     AuthModule,
     ExampleEntityModule,
     PrismaModule,
+    FirebaseModule,
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       ignoreEnvFile: process.env.NODE_ENV === 'production',
