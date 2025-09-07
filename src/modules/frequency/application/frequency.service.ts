d855053ca566908a503e9f1c151d3e28480b3cef
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-07 15:35:29 -0300

    feat(frequency): implement FrequencyService with getUserClasses

---
+++ src/modules/frequency/application/frequency.service.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { FREQUENCY_QUERIES_TOKEN, IFrequencyQueries } from "./frequency.service.query.interfaces";
+import { GetMyClassesDTO } from "./frequency.dtos";
+
+@Injectable()
+export class FrequencyService{
+    @Inject(FREQUENCY_QUERIES_TOKEN)
+    private readonly frequencyQueryService: IFrequencyQueries;
+    public async getUserClasses(userId: number): Promise<GetMyClassesDTO>{
+        var userClasses = await this.frequencyQueryService.getMyClasses(userId);
+        userClasses.push({
+            classId: null,
+            className: "Geral",
+            classState: "ATIVA",
+            levelName: null,
+            isGeral: true,
+            activity: {
+                activityId: null,
+                activityName: "Atividade geral",
+            },
+        })
+        return {
+            classes : userClasses
+        };
+    }
+}
