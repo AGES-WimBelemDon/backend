c3c67950d9f09928494b150d0e10997c1d70bc99
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-10 16:25:51 -0300

    feat(frequency): add new query using Prisma

--- src/modules/frequency/infrastructure/frequency.query.service.prisma.ts
+++ src/modules/frequency/infrastructure/frequency.query.service.prisma.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { IFrequencyQueries } from "../application/frequency.service.query.interfaces";
-import { UserClassesDTO } from "../application/frequency.dtos";
-import { FrequencyDTOMapper } from "./frequency.dto.mapper";
+import { UserClassesDTO, StudentGeneralFrequencyDTO } from "../application/frequency.dtos";
+import { FrequencyDTOMapper,PrismaStudentGeneralFrequency } from "./frequency.dto.mapper";
 
 @Injectable()
 export class PrismaFrequencyQueryService implements IFrequencyQueries {
@@ -38,5 +38,68 @@ export class PrismaFrequencyQueryService implements IFrequencyQueries {
       return [];
     }
     return user.classes.map(cls => FrequencyDTOMapper.toUserClassesDTO(cls));
+  }
+  async getGeneralFrequency(date: Date): Promise<StudentGeneralFrequencyDTO[]>{
+    const result = await this.prisma.$queryRaw<PrismaStudentGeneralFrequency[]>`
+      WITH 
+      freq_query AS (
+          SELECT
+              id_student AS id,
+              date,
+              'true' AS register_by_another_class,
+              'PRESENTE' AS status
+          FROM frequency
+          WHERE
+              date = ${date}
+              AND status = 'PRESENTE'
+              AND id_class IS NOT NULL
+          GROUP BY id_student, date
+
+          UNION
+          
+          SELECT
+              id_student AS id,
+              date,
+              'false' AS register_by_another_class,
+              'PRESENTE' AS status
+          FROM frequency
+          WHERE
+              date = ${date}
+              AND status = 'PRESENTE'
+              AND id_class IS NULL
+          GROUP BY id_student, date
+      ),
+      active_students AS (
+          SELECT
+              id,
+              full_name
+          FROM
+              student
+          WHERE
+              status = 'ATIVO'
+      )
+      SELECT
+          acs.id AS idStudent,
+          acs.full_name AS fullName,
+          CASE
+              WHEN fq.date IS NULL THEN ${date}
+              ELSE fq.date
+          END AS date,
+          CASE
+              WHEN fq.register_by_another_class IS NULL THEN 'false'
+              ELSE fq.register_by_another_class
+          END AS registerByAnotherClass,
+          CASE
+              WHEN fq.status IS NULL THEN 'AUSENTE'
+              ELSE fq.status
+          END AS status
+      FROM active_students AS acs
+      LEFT JOIN freq_query AS fq
+      ON acs.id = fq.id
+    `;
+    if(!result){
+      return []
+    }
+    return result.map(arg=>FrequencyDTOMapper.toStudentGeneralFrequencyDTO(arg));
   }
 }
