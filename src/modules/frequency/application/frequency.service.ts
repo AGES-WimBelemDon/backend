1b2d1edf1d287cdef92c4fc5e5a6774a294b4f68
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-12 20:18:24 -0300

    chore(style): add linter configuration and apply linting

--- src/modules/frequency/application/frequency.service.ts
+++ src/modules/frequency/application/frequency.service.ts
@@ -1,30 +1,51 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { FREQUENCY_QUERIES_TOKEN, IFrequencyQueries } from "./frequency.service.query.interfaces";
-import { UserClassesResponseDTO, StudentGeneralAttendanceResponseDTO } from "./frequency.dtos";
+import {
+  FREQUENCY_QUERIES_TOKEN,
+  IFrequencyQueries,
+} from "./frequency.service.query.interfaces";
+import {
+  UserClassesResponseDTO,
+  StudentGeneralAttendanceResponseDTO,
+  UpdateGeneralAttendanceRequestDTO,
+  UpdateGeneralAttendanceItemDTO,
+} from "./frequency.dtos";
 
 @Injectable()
-export class FrequencyService{
-    @Inject(FREQUENCY_QUERIES_TOKEN)
-    private readonly frequencyQueryService: IFrequencyQueries;
-    public async getUserClasses(userId: number): Promise<UserClassesResponseDTO>{
-        var userClasses = await this.frequencyQueryService.getMyClasses(userId);
-        userClasses.push({
-            classId: null,
-            className: "Geral",
-            classState: "ATIVA",
-            levelName: null,
-            isGeral: true,
-            activity: {
-                activityId: null,
-                activityName: "Atividade geral",
-            },
-        })
-        return {
-            classes : userClasses
-        };
-    }
-    public async getGeneralAttendance(date: Date): Promise<StudentGeneralAttendanceResponseDTO[]>{
-        const studentList = await this.frequencyQueryService.getGeneralAttendance(date);
-        return studentList;
-    }
+export class FrequencyService {
+  @Inject(FREQUENCY_QUERIES_TOKEN)
+  private readonly frequencyQueryService: IFrequencyQueries;
+  public async getUserClasses(userId: number): Promise<UserClassesResponseDTO> {
+    const userClasses = await this.frequencyQueryService.getMyClasses(userId);
+    userClasses.push({
+      classId: null,
+      className: "Geral",
+      classState: "ATIVA",
+      levelName: null,
+      isGeral: true,
+      activity: {
+        activityId: null,
+        activityName: "Atividade geral",
+      },
+    });
+    return {
+      classes: userClasses,
+    };
+  }
+  public async getGeneralAttendance(
+    date: Date,
+  ): Promise<StudentGeneralAttendanceResponseDTO[]> {
+    const studentList =
+      await this.frequencyQueryService.getGeneralAttendance(date);
+    return studentList;
+  }
+  public async updateGeneralAttendance(
+    array: UpdateGeneralAttendanceRequestDTO,
+  ): Promise<null> {
+    var validItems: UpdateGeneralAttendanceItemDTO[] = [];
+    if(array["updates"]){
+        validItems = array["updates"].filter(item=>item.generalAttendanceAllowed===true)
+    };
+    console.log(validItems);
+    return null;
+  }
 }
