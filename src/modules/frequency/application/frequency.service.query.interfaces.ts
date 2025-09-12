21fa93774d49de7947adec7608d23cb1fb481fdb
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-12 20:17:54 -0300

    chore(style): add linter configuration and apply linting

--- src/modules/frequency/application/frequency.service.query.interfaces.ts
+++ src/modules/frequency/application/frequency.service.query.interfaces.ts
@@ -1,7 +1,12 @@
-import { StudentGeneralAttendanceResponseDTO, UserClassesDTO } from "./frequency.dtos";
+import {
+  StudentGeneralAttendanceResponseDTO,
+  UserClassesDTO,
+} from "./frequency.dtos";
 
 export const FREQUENCY_QUERIES_TOKEN = "IFrequencyQueries";
-export interface IFrequencyQueries{
-    getMyClasses(userId: number): Promise<UserClassesDTO[]>;
-    getGeneralAttendance(date: Date): Promise<StudentGeneralAttendanceResponseDTO[]>;
+export interface IFrequencyQueries {
+  getMyClasses(userId: number): Promise<UserClassesDTO[]>;
+  getGeneralAttendance(
+    date: Date,
+  ): Promise<StudentGeneralAttendanceResponseDTO[]>;
 }
