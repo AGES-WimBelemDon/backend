0225e5eb32dc3043e0b169d02889088a06b6366b
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-13 11:13:09 -0300

    feat: create the deleteManyByStudentAndClassAndDate.

--- src/modules/frequency/domain/frequency.repository.ts
+++ src/modules/frequency/domain/frequency.repository.ts
@@ -1,3 +1,5 @@
+import { Frequency } from "./frequency.entity";
+export const FREQUENCY_REPOSITORY_TOKEN = "IFrequencyRepository";
 export interface IFrequencyRepository{
-    
+    deleteManyByStudentAndClassAndDate(frequencies: Frequency[]): Promise<boolean>;
 }
