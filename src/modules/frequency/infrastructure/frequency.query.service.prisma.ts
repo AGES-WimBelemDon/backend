292b9ae23d991a073226f17891afe1d7c7c351ba
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-09-14 11:11:53 -0300

    chore: implement getStudentAttendanceDetails.

--- src/modules/frequency/infrastructure/frequency.query.service.prisma.ts
+++ src/modules/frequency/infrastructure/frequency.query.service.prisma.ts
@@ -106,4 +106,53 @@ export class PrismaFrequencyQueryService implements IFrequencyQueries {
     const attendanceArray = result.map(arg=>FrequencyDTOMapper.toStudentGeneralAttendanceDTO(arg));
     return attendanceArray;
   }
+  async getStudentAttendanceDetails(classId: number, date: Date) {
+    type AttendanceResult = {
+      frequencyId: number;
+      stutendId: number;
+      fullName: string;
+      attendance: number;
+      status: string;
+      notes: string | null;
+    }
+
+    const results = await this.prisma.$queryRaw<AttendanceResult[]>`
+        WITH
+        total_classes AS (
+          SELECT COUNT(DISTINCT date) AS count
+          FROM frequency
+          WHERE id_class = ${classId}
+        ),
+        student_attendance AS (
+          SELECT
+            id_student,
+            COUNT(*) AS present_count
+          FROM frequency
+          WHERE id_class = ${classId}
+          AND status = 'PRESENTE'
+          GROUP BY id_student
+        ),
+        daily_attendance AS (
+          SELECT
+            id,
+            id_student,
+            status,
+            notes
+          FROM frequency
+          WHERE id_class = ${classId}
+          AND date = ${date}
+        )
+        SELECT
+          d.id AS "frequencyId",
+          s.id AS "stutendId",
+          s.full_name AS "fullName",
+          COALESCE(a.present_count, 0)/CAST((SELECT count FROM total_classes) AS DECIMAL(4,2)) AS attendance,
+          d.status,
+          d.notes
+        FROM daily_attendance d
+        JOIN student s ON s.id = d.id_student
+        LEFT JOIN student_attendance a ON a.id_student = d.id_student
+      `;
+    return results;
+  }
 }
