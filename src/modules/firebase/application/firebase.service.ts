ef6ca1efabfbe9542d32318b81f18911cb407b6a
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-10-18 18:49:16 -0300

    feat(firebase-admin): services to create user with email, delete it and verify token validity

--- src/modules/firebase/application/firebase.service.ts
+++ src/modules/firebase/application/firebase.service.ts
@@ -2,23 +2,61 @@ import { BadRequestException, Inject, Injectable } from "@nestjs/common";
 import * as admin from "firebase-admin";
 import { FIREBASE_ADMIN } from "../firebase.config.module";
 import { CreateExampleEntityDTO } from "src/modules/exampleEntity/application/create-exampleEntity.dto";
+import { RegisterUserDTO } from "src/modules/user/application/user.dtos";
 
 @Injectable()
-export class FirebaseService{
-    
-    constructor(
-    @Inject(FIREBASE_ADMIN) private readonly firebaseAdmin: admin.app.App
-  ) {};
-    async createExampleEntityOnFirebase(userData: CreateExampleEntityDTO): Promise<{
-      user: admin.auth.UserRecord;
-    }> {
-      try {
-        const userRecord = await this.firebaseAdmin.auth().createUser({
-        email: userData.email
+export class FirebaseService {
+  constructor(
+    @Inject(FIREBASE_ADMIN) private readonly firebaseAdmin: admin.app.App,
+  ) {}
+
+  async createFirebaseUser(
+    user: RegisterUserDTO,
+  ): Promise<admin.auth.UserRecord> {
+    try {
+      const userRecord = await this.firebaseAdmin.auth().createUser({
+        email: user.email,
+        displayName: user.name,
       });
-      return { user : userRecord }; 
-      } catch (error) {
-        throw new BadRequestException(error);
-      };
+      return userRecord;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async createExampleEntityOnFirebase(
+    userData: CreateExampleEntityDTO,
+  ): Promise<{
+    user: admin.auth.UserRecord;
+  }> {
+    try {
+      const userRecord = await this.firebaseAdmin.auth().createUser({
+        email: userData.email,
+      });
+      return { user: userRecord };
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async deleteFirebaseUser(uid: string): Promise<void> {
+    try {
+      await this.firebaseAdmin.auth().deleteUser(uid);
+    } catch (error) {
+      throw new BadRequestException(
+        "Failed to delete Firebase user: " + error.message,
+      );
+    }
+  }
+
+  async verifyIdToken(token: string): Promise<admin.auth.DecodedIdToken> {
+    try {
+      const decodedToken = await this.firebaseAdmin.auth().verifyIdToken(token);
+      return decodedToken;
+    } catch (error) {
+      throw new BadRequestException(
+        "Invalid or expired Firebase token: " + error.message,
+      );
+    }
   }
 }
