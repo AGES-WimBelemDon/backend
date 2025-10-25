4eecf02db35abdaa8456e05af450a24aa97f3a59
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-10-25 15:38:12 -0300

    feat(class): added a query service in the class resource

--- src/modules/class/class.module.ts
+++ src/modules/class/class.module.ts
@@ -1,14 +1,19 @@
 import { Module } from "@nestjs/common";
 import { ClassController } from "./presentation/class.controller";
 import { ClassService } from "./application/class.service";
-import { PrismaModule } from "src/prisma/prisma.module";
 import { LevelModule } from "../level/level.module";
+import { CLASS_QUERIES_TOKEN } from "./application/class.service.query.interfaces";
+import { ClassQueryServicePrisma } from "./infrastructure/class.query.service.prisma";
 
 @Module({
     imports: [LevelModule],
     controllers: [ClassController],
     providers: [
         ClassService,
+        {
+            provide : CLASS_QUERIES_TOKEN,
+            useClass: ClassQueryServicePrisma
+        }
     ]
 })
 export class ClassModule {}
