743b29418f8f15ebb4605a182ed918e4b0df4c05
Felipe Frison <felipe.frison@edu.pucrs.br>
2025-11-07 11:30:28 -0300

    chore(dto): create mime type enum

---
+++ src/common/enums/mime-type.enum.ts
@@ -0,0 +1,11 @@
+export enum MimeTypes {
+  IMAGE_JPEG = "image/jpeg",
+  IMAGE_PNG = "image/png",
+  IMAGE_GIF = "image/gif",
+  IMAGE_WEBP = "image/webp",
+  IMAGE_SVG = "image/svg+xml",
+  APPLICATION_PDF = "application/pdf",
+  TEXT_PLAIN = "text/plain",
+  TEXT_CSV = "text/csv",
+  APPLICATION_ZIP = "application/zip"
+}
